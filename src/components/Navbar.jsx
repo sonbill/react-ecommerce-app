944c2b2e1f705b41bf1dbbe38120b5df8c91a80d
Sơn Nguyễn <61671615+sonbill@users.noreply.github.com>
2022-04-25 14:21:41 +0700

    first setup

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Container = styled.div`
+  height:60px;
+  background-color: black;
+  color: white;
+`
+
+function Navbar() {
+  return (
+    <Container>
+      Navbar
+    </Container>
+  )
+}
+
+export default Navbar
